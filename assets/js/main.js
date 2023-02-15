247fabf928b056ea7693f183f359fcbef6684625
daelvinci <119412720+daelvinci@users.noreply.github.com>
2023-02-16 00:57:01 +0400

    uptade

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ btn.onclick= (() =>{
     let summary=Number(document.getElementById('summary').value)
     let percentage=Number(document.getElementById('percentage').value)
     let time=Number(document.getElementById('time').value)
-    let sum=summary+(time*(summary*percentage/100))
+    let sum=summary+(time*(summary*(percentage/time)/100))
     document.getElementById("sum").value=`  Umumi odenilecek mebleg: ${sum}`;
     document.getElementById('monthlyAmount').value=`  Ayliq odenis: ${(summary/12).toFixed(2)}`;
 })
